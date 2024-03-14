9ae7519eaed7bb86416da7e5630ccda9bc4eb52e
Depriest43 <depriest43@gmail.com>
2024-03-14 16:05:43 -0700

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace CSIPracticeAssignment1_DesmondRagsdale
+{
+    internal class Class1
+    {
+        //fields
+        private string_make;
+        private string_model;
+            private int _year;
+        private string _color;
+        private string _mileage;
+
+        //properties
+        public string Make { get => _make; set => _make = value; }
+        public string Model { get => _model; set => _model = value; }
+        public int Year { get => _year; set => _year = value; }
+        public string Color { get => _color; set => _color = value; }
+        public int Mileage { get => _mileage; set => _mileage = value; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSIPracticeAssignment1_DesmondRagsdale
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Createcar details
+            car myCar = new car();
+
+            //Setting Properties
+
+            myCar.Make = "Toyota";
+            myCar.Model = "Camry";
+            myCar.Year = 2020;
+            myCar.Color = "Blue";
+            myCar.Mileage = 5000;
+
+            //Showing car details
+
+            Console.WriteLine("Car Details:");
+            Console.WriteLine($"Make: {myCar.Make}");
+            Console.WriteLine($"Model: {myCar.Model}");
+            Console.WriteLine($"Year: {myCar.Year}");
+            Console.WriteLine($"Color: {myCar.Color}");
+            Console.WriteLine($"Mileage: {myCar.Mileage}");
+        }
+    }
+}
